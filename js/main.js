a3736bec1bdf40b4daad5e1e5c4b80c7e825d379
Your Namemauler2007 <mauler2007@gmail.com>
2021-12-18 18:04:12 +0200

    finish todolist

--- js/main.js
+++ js/main.js
@@ -1,76 +1,92 @@
 "use strict"
-// console.log("123");
-const todoControl = document.querySelector('.todo-control');
-const headerInput = document.querySelector('.header-input');
+document.addEventListener('DOMContentLoaded', function () {
 
-const todoList = document.querySelector('.todo-list');
-const todoCompleted = document.querySelector('.todo-completed');
+  // console.log("123");
+  const todoControl = document.querySelector('.todo-control');
+  const headerInput = document.querySelector('.header-input');
 
-const toDoData = [
+  const todoList = document.querySelector('.todo-list');
+  const todoCompleted = document.querySelector('.todo-completed');
 
-]
+  const toDoData = []
 
-const render = function () {
-  todoList.innerHTML = ''
-  todoCompleted.innerHTML = ''
-  // console.log(toDoData);
-  toDoData.forEach(function (currentItem, index, array) {
-    // console.log(currentItem);
-    let li = document.createElement('li')
+  const pushToStorage = function (data) {
+    let dataStr = JSON.stringify(data);
 
-    li.classList.add('todo-item')
-
-    if (currentItem.text == '') {
-      console.log(' не заполнена  форма ');
-      return;
-    } else {
-      li.innerHTML = '<span class="text-todo">' + currentItem.text + '</span>' +
-        '<div class="todo-buttons">' +
-        '<button class="todo-remove"></button>' +
-        '<button class="todo-complete"></button>' +
-        '</div>';
-    }
-
-    if (li.innerHTML == '') {
-      console.log(' не   добавлять в массив ничего ');
-      return;
-    } else if (currentItem.completed) {
-      todoCompleted.append(li)
-    } else {
-      todoList.append(li)
-    }
-
-
-
-
-    li.querySelector('.todo-complete').addEventListener('click', function () {
-      currentItem.completed = !currentItem.completed
-      render()
-    })
-
-    li.querySelector('.todo-remove').addEventListener('click', function (event) {
-      array.splice(index, 1)
-      render()
-    })
-  });
-  console.log(toDoData);
-}
-
-todoControl.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  const newToDo = {
-    text: headerInput.value,
-    completed: false
+    localStorage.setItem('list', dataStr);
   }
 
-  toDoData.push(newToDo)
-  headerInput.value = '';
+  const render = function () {
+    
+    todoList.innerHTML = ''
+    todoCompleted.innerHTML = ''
 
-  render()
+    // console.log(toDoData);
+    toDoData.forEach(function (currentItem, index, array) {
+      // console.log(currentItem);
+      let li = document.createElement('li')
 
-  // console.log('e.preventDefault-=ON')
+      li.classList.add('todo-item')
+
+      if (currentItem.text == '') {
+        console.log(' не заполнена  форма ');
+        return;
+      } else {
+        li.innerHTML = '<span class="text-todo">' + currentItem.text + '</span>' +
+          '<div class="todo-buttons">' +
+          '<button class="todo-remove"></button>' +
+          '<button class="todo-complete"></button>' +
+          '</div>';
+      }
+
+      if (li.innerHTML == '') {
+        console.log(' не   добавлять в массив ничего ');
+        return;
+      } else if (currentItem.completed) {
+        todoCompleted.append(li)
+      } else {
+        todoList.append(li)
+      }
+
+      li.querySelector('.todo-complete').addEventListener('click', function () {
+        currentItem.completed = !currentItem.completed
+        render()
+      })
+
+      li.querySelector('.todo-remove').addEventListener('click', function (event) {
+        array.splice(index, 1)
+        render()
+      })
+    });
+
+    pushToStorage(toDoData);
+  }
+
+  if (localStorage.list) {
+
+    let objFromLocal = JSON.parse(localStorage.list)
+
+    for (let i = 0; i < objFromLocal.length; i++) {
+      toDoData.push(objFromLocal[i])
+      render();
+    }
+  }
+
+  todoControl.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    if (headerInput.value == '') {
+      return;
+    }
+
+    const newToDo = {
+      text: headerInput.value,
+      completed: false
+    }
+
+    toDoData.push(newToDo)
+    headerInput.value = '';
+
+    render()
+  });
 });
-
-// console.log(todoControl);
-
